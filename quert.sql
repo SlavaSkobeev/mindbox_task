165870b2c19bedc54b495283a0fad4ac9e65083e
SlavaSkobeev <64376486+SlavaSkobeev@users.noreply.github.com>
2020-05-11 14:16:43 +0300

    Create quert.sql

---
+++ quert.sql
@@ -0,0 +1,10 @@
+/* 
+В БД находятся 3 таблицы 
+1) Categories, со следующими полями id(уникальный идентификатор категории), nameCat (имя категории)
+2) Product, со следующими полями id(уникальный идентификатор продукта), nameProd (имя продукта)
+3) ProdToCat, со следующими полями idProd(внешний ключ к Product), idCat (внешний ключ к Categories)
+*/
+
+SELECT pr.NameCat
+FROM Product pr LEFT JOIN ProdToCat ptc ON pr.id = ptc.idProd
+                LEFT JOIN Categories cat ON cat.id=ptc.idCat
